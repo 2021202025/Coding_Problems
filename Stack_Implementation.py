c8cd5e39907006d6ba9aef851bdfda1b0dba36e4
ShadZire <gmanu21097@gmail.com>
2019-07-07 21:20:31 +0530

    Basic Stack

---
+++ Stack_Implementation.py
@@ -0,0 +1,25 @@
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+s = Stack()
+
+s.push(5)
+print(s.isEmpty())
+
